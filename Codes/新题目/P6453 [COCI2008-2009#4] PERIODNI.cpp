233633ac0da4ef4d91851eec294a678b6d12f4d0
ReClouds <recalling_clouds@126.com>
2022-06-22 19:50:40 +0800

    Site updated: 2022-06-22 19:50:40

---
+++ Codes/新题目/P6453 [COCI2008-2009#4] PERIODNI.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cctype>
+
+using namespace std;
+
+namespace IO
+{
+    int f, c;
+
+    template<typename T> inline void _Read(T &k)
+    {
+        k = 0, f = 1, c = getchar();
+        while(!isdigit(c))
+        {
+            if(c == '-') f = -1;
+            c = getchar();
+        }
+        while(isdigit(c))
+        {
+            k = (k << 3) + (k << 1) + c - '0';
+            c = getchar();
+        }
+        return k *= f, void();
+    }
+
+    template<typename T> inline void _Write(T k)
+    {
+        if(k < 0) putchar('-'), k = -k;
+        if(k > 9) _Write(k / 10);
+        return putchar(k % 10 + '0'), void();
+    }
+
+    inline int Read32() { int k; _Read(k); return k; }
+    inline void Write32(int k, char ed = '\n') { return _Write(k), putchar(ed), void(); }
+}
+
+using IO :: Read32;
+using IO :: Write32;
+
+namespace Program
+{
+    const int MOD = 1000000007;
+
+    inline int Run()
+    {
+        return 0;
+    }
+}
+
+int main() { return Program :: Run(); }
+/*
+
+                                                                 /@@@O
+                                                              =@@@@@@@@@
+                                                           ]/@@@@@@@@@@@
+                                             ]]]]]/@@@@@@@@@@@@@@@@@@@@@
+                       @@@@@\]]]]]]]]@@@@@@@@@@OO/          /@@@@@@@@@@@@
+                      =@@@@@@@@@@O/[[                    [ @@@O[     O@@@\
+                      =@@@@@@@@@@@]                     /@@/            \@\
+                      =@@@@@      O@@@@\]]           /@@@[               \@@\
+                      @@@@@             /@@@@@\    /@@[                    O@O
+                     =@@ =@                  [\@@@@@\]                       @@O
+                     @@/ O@                   /@@   [O@@@@\]                  =@@
+                    /@   =@@               /@@/            @@@@@OO [            @@]
+                   /@@    @@             @@@                     \@@@@O]         \@@
+                   @@     @@         =O@@/                             \@@@@O/O    @@@\
+                  @@      =@       /@@/                                      \@@@@@@@@@@@@
+                =/@@      =@     @@@/                                             \@@@@@@@
+                =@@       =@@ O@@O                                                  =@@@@@
+                @@         @@@@[
+              =/@O       O@@@
+              =@@     \@@@ \@
+              @@    @@@/   =@\
+            =@@/[]@@@[     =@@
+           /@@@@@@          @@
+         /@@@@@@@           @@
+         @@@@@@@            =@
+          \@@@@@@\          =@
+             @@@@@@@\       =@@
+               @@\[@@@       @@
+                =@@  \@@\    @@
+                 =@@/  [@@@  @@
+                  =@@\    =@@@@O
+                    \@@     =@@@]                                                    /@@@O
+                     \@@     =@@@@\                                                @@@@@@@
+                      =@@     @@ [@@@                                        ]]@@@@@@@@@@@
+                        @@    =@     @@\                              ]]@@@@@/[ /O@@@@[
+                         \@@  =@\      \@@\                   \]@@@@@@O[       O@@[
+                          =@@ /@@         @@O          ]/@@@@@@/[           /@@/
+                           \@@@@@@          O@@]/O@@@@@/[                ]O@@/
+                           =@@@@@@@@\]]]]\@@@@@@@O                     O@@
+                           =@@@@@@@@@@@@@/       O@@\                @@@
+                           =@@@@@@@@@@@@]          [@@\           /@@O
+                            @@@@@@@@@@@@@@@@@@@O\]    \@@@O  ]]/@@@[
+                              [@@/             [O@@@@@@@@@@@@@@@@/
+                                                       \@@@@@@@@
+                                                          \@@@@
+*/
