e7cad56f9585b8cba195aa36a072cc642a8add17
ReClouds <recalling_clouds@126.com>
2022-06-23 20:40:49 +0800

    Site updated: 2022-06-23 20:40:49

--- Codes/新题目/AGC026D Histogram Coloring.cpp
+++ Codes/新题目/AGC026D Histogram Coloring.cpp
@@ -2,6 +2,7 @@
 #include<cstdio>
 #include<cstdlib>
 #include<cctype>
+#include<algorithm>
 #include<vector>
 
 using namespace std;
@@ -42,11 +43,10 @@ using IO :: Write32;
 
 namespace Program
 {
-    const int MAXN = 505;
-    const int MAXM = 1000005;
+    const int MAXN = 105;
     const int MOD = 1000000007;
 
-    int n, k, rt, tot, h[MAXN], fac[MAXM], invf[MAXM], si[MAXN], f[MAXN << 1][MAXN];
+    int n, rt, tot, h[MAXN], si[MAXN], f[MAXN << 1][2];
 
     inline int Get_MOD(int k) { return k - (k >= MOD) * MOD; }
 
@@ -62,54 +62,36 @@ namespace Program
         return res;
     }
 
-    inline int C(int n, int m)
-    { 
-        if(n < m) return 0;
-        return 1LL * fac[n] * invf[m] % MOD * invf[n - m] % MOD; 
-    }
-
-    inline int W(int n, int m, int k) { return 1LL * C(n, k) * C(m, k) % MOD * fac[k] % MOD; }
-
     inline void Solve(int &u, int l, int r, int lst)
     {
-        u = ++tot, f[u][0] = 1;
+        u = ++tot, f[u][0] = f[u][1] = 1;
         int mi = 0x3F3F3F3F;
         for(register int i = l; i <= r; i++) mi = min(mi, h[i]);
         vector<int> pos;
         for(register int i = l; i <= r; i++) if(h[i] == mi) pos.push_back(i);
-        si[u] = (int)(pos.size()), pos.push_back(r + 1);
-        int pre = l - 1;
+        int cnt = (int)(pos.size()), pre = l - 1;
+        si[u] = cnt, pos.push_back(r + 1);
         for(auto it : pos)
         {
             if(pre + 1 <= it - 1)
             {
                 int v = 0;
                 Solve(v, pre + 1, it - 1, mi), si[u] += si[v];
-                for(register int i = k; i >= 0; i--)
-                {
-                    int val = 0;
-                    for(register int j = 0; j <= i; j++) val = Get_MOD(val + 1LL * f[u][j] * f[v][i - j] % MOD);
-                    f[u][i] = val;
-                }
+                f[u][0] = 1LL * f[u][0] * Get_MOD(f[v][0] + f[v][1]) % MOD;
+                f[u][1] = 1LL * f[u][1] * f[v][1] % MOD;
             }
             pre = it;
         }
-        for(register int i = k; i >= 0; i--)
-        {
-            int val = 0;
-            for(register int j = 0; j <= i; j++) val = Get_MOD(val + 1LL * f[u][j] * W(si[u] - j, mi - lst, i - j) % MOD);
-            f[u][i] = val;
-        }
+        f[u][0] = Get_MOD(1LL * f[u][0] * Pow(2, cnt) % MOD + 1LL * Get_MOD(Pow(2, mi - lst) - 2 + MOD) * f[u][1] % MOD);
+        f[u][1] = 1LL * f[u][1] * Pow(2, mi - lst) % MOD;
         return;
     }
 
     inline int Run()
     {
-        n = Read32(), k = Read32();
+        n = Read32();
         for(register int i = 1; i <= n; i++) h[i] = Read32();
-        fac[0] = 1; for(register int i = 1; i <= MAXM - 5; i++) fac[i] = 1LL * fac[i - 1] * i % MOD;
-        invf[MAXM - 5] = Pow(fac[MAXM - 5], MOD - 2); for(register int i = MAXM - 6; i >= 0; i--) invf[i] = 1LL * invf[i + 1] * (i + 1) % MOD;
-        return Solve(rt, 1, n, 0), Write32(f[rt][k]), 0;
+        return Solve(rt, 1, n, 0), Write32(f[rt][0]), 0;
     }
 }
 
