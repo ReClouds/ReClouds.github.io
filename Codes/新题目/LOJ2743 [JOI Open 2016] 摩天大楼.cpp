db4c90d5b28d94d868b3dc230e89364bf7b37d60
ReClouds <recalling_clouds@126.com>
2022-06-24 09:58:30 +0800

    Site updated: 2022-06-24 09:58:30

--- Codes/新题目/LOJ2743 [JOI Open 2016] 摩天大楼.cpp
+++ Codes/新题目/LOJ2743 [JOI Open 2016] 摩天大楼.cpp
@@ -2,6 +2,7 @@
 #include<cstdio>
 #include<cstdlib>
 #include<cctype>
+#include<cstring>
 #include<algorithm>
 
 using namespace std;
@@ -43,9 +44,10 @@ using IO :: Write32;
 namespace Program
 {
     const int MAXN = 105;
+    const int MAXM = 1005;
     const int MOD = 1000000007;
 
-    int n, l, a[MAXN];
+    int n, l, a[MAXN], f[2][MAXN][3][MAXM];
 
     inline int Get_MOD(int k) { return k - (k >= MOD) * MOD; }
 
@@ -53,8 +55,33 @@ namespace Program
     {
         n = Read32(), l = Read32();
         for(register int i = 1; i <= n; i++) a[i] = Read32();
+        if(n == 1) return puts("1"), 0;
         sort(a + 1, a + n + 1);
-        return 0;
+        int u = 0, v = 1;
+        f[u][1][2][0] = 1, f[u][1][1][0] = 2;
+        for(register int i = 2; i <= n; i++)
+        {
+            memset(f[v], 0, sizeof f[v]);
+            for(register int j = 1; j <= i; j++)
+            {
+                for(register int k = 0; k <= 2; k++)
+                {
+                    for(register int t = 0; t <= l; t++) if(f[u][j][k][t] && t + (a[i] - a[i - 1]) * ((j - 1) * 2 + k) <= l)
+                    {
+                        int w = t + (a[i] - a[i - 1]) * ((j - 1) * 2 + k);
+                        f[v][j + 1][k][w] = Get_MOD(f[v][j + 1][k][w] + 1LL * f[u][j][k][t] * (j - 1 + k) % MOD);
+                        if(k) f[v][j + 1][k - 1][w] = Get_MOD(f[v][j + 1][k - 1][w] + 1LL * f[u][j][k][t] * k % MOD);
+                        f[v][j][k][w] = Get_MOD(f[v][j][k][w] + 1LL * f[u][j][k][t] * ((j - 1) * 2 + k) % MOD);
+                        if(k) f[v][j][k - 1][w] = Get_MOD(f[v][j][k - 1][w] + 1LL * f[u][j][k][t] * k % MOD);
+                        f[v][j - 1][k][w] = Get_MOD(f[v][j - 1][k][w] + 1LL * f[u][j][k][t] * (j - 1) % MOD);
+                    }
+                }
+            }
+            swap(u, v);
+        }
+        int ans = 0;
+        for(register int i = 0; i <= l; i++) ans = Get_MOD(ans + f[u][1][0][i]);
+        return Write32(ans), 0;
     }
 }
 
