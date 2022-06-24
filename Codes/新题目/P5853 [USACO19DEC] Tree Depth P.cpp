ba4122912eaaa29204ff4f2fcf67cc20c5264462
ReClouds <recalling_clouds@126.com>
2022-06-24 21:59:01 +0800

    Site updated: 2022-06-24 21:58:59

---
+++ Codes/新题目/P5853 [USACO19DEC] Tree Depth P.cpp
@@ -0,0 +1,141 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cctype>
+#include<cstring>
+
+using namespace std;
+
+namespace IO
+{
+    int f, c;
+
+    template<typename T> inline void _Read(T &k)
+    {
+        k = 0, f = 1, c = getchar();
+        while(!isdigit(c))
+        {
+            if(c == '-') f = -1;
+            c = getchar();
+        }
+        while(isdigit(c))
+        {
+            k = (k << 3) + (k << 1) + c - '0';
+            c = getchar();
+        }
+        return k *= f, void();
+    }
+
+    template<typename T> inline void _Write(T k)
+    {
+        if(k < 0) putchar('-'), k = -k;
+        if(k > 9) _Write(k / 10);
+        return putchar(k % 10 + '0'), void();
+    }
+
+    inline int Read32() { int k; _Read(k); return k; }
+    inline void Write32(int k, char ed = '\n') { return _Write(k), putchar(ed), void(); }
+}
+
+using IO :: Read32;
+using IO :: Write32;
+
+namespace Program
+{
+    const int MAXN = 305;
+    const int MAXM = 50005;
+
+    int n, k, m, cnt, u, v, f[2][MAXM], g[MAXM], ans[MAXN];
+
+    inline int Get_MOD(int t) { return t - (t >= m) * m; }
+
+    inline void Undo(int t)
+    {
+        memset(g, 0, sizeof g);
+        int val = m - 1; g[0] = 1;
+        for(register int i = 1; i <= cnt - t; i++)
+        {
+            if(i > t) val = Get_MOD(val + g[i - t - 1]);
+            val = Get_MOD(val - (g[i] = Get_MOD(f[u][i] + val)) + m);
+        }
+        return;
+    }
+
+    inline int Run()
+    {
+        n = Read32(), k = Read32(), m = Read32();
+        u = 0, v = 1, f[u][0] = 1;
+        for(register int i = 1; i <= n; i++)
+        {
+            f[v][0] = 1, cnt += i - 1;
+            for(register int j = 1; j <= cnt; j++)
+            {
+                f[v][j] = Get_MOD(f[v][j - 1] + f[u][j]);
+                if(j >= i) f[v][j] = Get_MOD(f[v][j] - f[u][j - i] + m);
+            }
+            swap(u, v);
+        }
+        for(register int i = 1; i <= n; i++) ans[i] = f[u][k];
+        for(register int i = 1; i < n; i++)
+        {
+            Undo(i);
+            for(register int j = 1; j <= n - i; j++)
+            {
+                if(i <= k) ans[j] = Get_MOD(ans[j] + g[k - i]);
+                ans[j + i] = Get_MOD(ans[j + i] + g[k]);
+            }
+        }
+        for(register int i = 1; i <= n; i++) Write32(ans[i], " \n"[i == n]);
+        return 0;
+    }
+}
+
+int main() { return Program :: Run(); }
+/*
+
+                                                                 /@@@O
+                                                              =@@@@@@@@@
+                                                           ]/@@@@@@@@@@@
+                                             ]]]]]/@@@@@@@@@@@@@@@@@@@@@
+                       @@@@@\]]]]]]]]@@@@@@@@@@OO/          /@@@@@@@@@@@@
+                      =@@@@@@@@@@O/[[                    [ @@@O[     O@@@\
+                      =@@@@@@@@@@@]                     /@@/            \@\
+                      =@@@@@      O@@@@\]]           /@@@[               \@@\
+                      @@@@@             /@@@@@\    /@@[                    O@O
+                     =@@ =@                  [\@@@@@\]                       @@O
+                     @@/ O@                   /@@   [O@@@@\]                  =@@
+                    /@   =@@               /@@/            @@@@@OO [            @@]
+                   /@@    @@             @@@                     \@@@@O]         \@@
+                   @@     @@         =O@@/                             \@@@@O/O    @@@\
+                  @@      =@       /@@/                                      \@@@@@@@@@@@@
+                =/@@      =@     @@@/                                             \@@@@@@@
+                =@@       =@@ O@@O                                                  =@@@@@
+                @@         @@@@[
+              =/@O       O@@@
+              =@@     \@@@ \@
+              @@    @@@/   =@\
+            =@@/[]@@@[     =@@
+           /@@@@@@          @@
+         /@@@@@@@           @@
+         @@@@@@@            =@
+          \@@@@@@\          =@
+             @@@@@@@\       =@@
+               @@\[@@@       @@
+                =@@  \@@\    @@
+                 =@@/  [@@@  @@
+                  =@@\    =@@@@O
+                    \@@     =@@@]                                                    /@@@O
+                     \@@     =@@@@\                                                @@@@@@@
+                      =@@     @@ [@@@                                        ]]@@@@@@@@@@@
+                        @@    =@     @@\                              ]]@@@@@/[ /O@@@@[
+                         \@@  =@\      \@@\                   \]@@@@@@O[       O@@[
+                          =@@ /@@         @@O          ]/@@@@@@/[           /@@/
+                           \@@@@@@          O@@]/O@@@@@/[                ]O@@/
+                           =@@@@@@@@\]]]]\@@@@@@@O                     O@@
+                           =@@@@@@@@@@@@@/       O@@\                @@@
+                           =@@@@@@@@@@@@]          [@@\           /@@O
+                            @@@@@@@@@@@@@@@@@@@O\]    \@@@O  ]]/@@@[
+                              [@@/             [O@@@@@@@@@@@@@@@@/
+                                                       \@@@@@@@@
+                                                          \@@@@
+*/
